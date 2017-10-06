03eaffac17ec97ad5400b3995ed7e1db155be0e8
zloiy <norfolf1000@gmail.com>
2017-10-06 21:02:29 +0300

    feat(lab-1 service): add logger

--- src/app/lab-1/lab-1.service.ts
+++ src/app/lab-1/lab-1.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/timeInterval';
 import 'rxjs/add/observable/interval';
+import { NGXLogger } from 'ngx-logger';
 
 @Injectable()
 export class Lab1Service {
 
   interval = null;
 
-  constructor() {
+  constructor(private logger: NGXLogger) {
     this.interval = Observable
       .interval(1000 /*ms*/)
       .timeInterval();
@@ -19,7 +20,7 @@ export class Lab1Service {
     canvasContext.clearRect(0, 0, canvas.width, canvas.height);
     canvasContext.beginPath();
     for (let horizCoord = 0; horizCoord < canvas.width; horizCoord += Number(tileSize)) {
-      canvasContext.moveTo(horizCoord,0);
+      canvasContext.moveTo(horizCoord, 0);
       canvasContext.lineTo(horizCoord, canvas.height);
       canvasContext.stroke();
     }
@@ -37,23 +38,31 @@ export class Lab1Service {
     const dy = (line.y2 - line.y1) / length;
     let x = line.x1 + 0.5;
     let y = line.y1 + 0.5;
+    this.logger.info('Start drawing...');
+    this.logger.info(`x:${x}, y:${y}`);
     this.createTile(canvas, Math.floor(x) * tileSize, Math.floor(y) * tileSize, tileSize);
     let pointId = 0;
     if (debug) {
-      this.interval.subscribe(() => {
+     const subscription = this.interval.subscribe(() => {
         x = x + dx;
         y = y + dy;
+        this.logger.info(`x:${x}, y:${y}`);
         this.createTile(canvas, Math.floor(x) * tileSize, Math.floor(y) * tileSize, tileSize);
         pointId++;
-        pointId < length ? 0 : this.interval.unsubscribe();
+        if (pointId >= length) {
+          subscription.unsubscribe();
+          this.logger.info('End drawing.');
+        }
       });
     } else {
       while (pointId < length) {
         x = x + dx;
         y = y + dy;
+        this.logger.info(`x:${x}, y:${y}`);
         this.createTile(canvas, Math.floor(x) * tileSize, Math.floor(y) * tileSize, tileSize);
         pointId++;
       }
+      this.logger.info('End drawing.');
     }
   }
 
